54ce0ed863112d440154445dba0c83c380af79c6
idemirtas28 <idemirtas28@my.whitworth.edu>
2025-07-03 16:48:06 -0700

    Welcome to Litly!

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <header className="flex justify-center">
+        <h1 className="text-4xl font-semibold">Welcome to Litly!</h1>
+      </header>
+    </main>
+  );
+}
